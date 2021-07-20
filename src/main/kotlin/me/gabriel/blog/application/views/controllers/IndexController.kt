83b2c1cc188dbe0ba8893d1b0db398b1c6fbf389
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-20 17:22:42 -0400

    refactor: torna dinâmico menu de categorias

--- src/main/kotlin/me/gabriel/blog/application/views/controllers/IndexController.kt
+++ src/main/kotlin/me/gabriel/blog/application/views/controllers/IndexController.kt
@@ -1,8 +1,13 @@
 package me.gabriel.blog.application.views.controllers
 
+import me.gabriel.blog.core.ports.UseCaseHandler
+import me.gabriel.blog.core.usecases.category.FindAllCategoryInputValue
+import me.gabriel.blog.core.usecases.category.FindAllCategoryOutputValue
+import me.gabriel.blog.core.usecases.category.FindAllCategoryUseCase
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -12,13 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping
  */
 @Controller
 @RequestMapping("/")
-class IndexController {
+class IndexController(
+    private val useCaseHandler: UseCaseHandler,
+    private val findAllCategoryUseCase: FindAllCategoryUseCase
+) {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping
-    fun index(): String {
+    fun index(model: Model): String {
+
         logger.info("index()")
+
+        val categories = useCaseHandler.handle(
+            findAllCategoryUseCase,
+            FindAllCategoryInputValue(),
+            FindAllCategoryOutputValue::categories
+        )
+
+        model.addAttribute("categories", categories)
+
         return "index"
     }
 
